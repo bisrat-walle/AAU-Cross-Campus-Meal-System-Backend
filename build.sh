35c57e45021c78a11bb9c011a48f3f3c0091a2bb
bisrat-walle <bisratwalle3@gmail.com>
2023-03-12 19:39:48 +0300

    improved

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt --use-pep517
 
+sudo apt-get install zbar-tools
+
 python manage.py collectstatic --no-input
 python manage.py migrate
