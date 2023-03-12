89dcaa7fc713d59ecf4c7d41379c1efe114c0e9c
bisrat-walle <bisratwalle3@gmail.com>
2023-03-12 18:46:12 +0300

    improved

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -o errexit  # exit on error
 
-pip install -r requirements.txt
+pip install -r requirements.txt --use-pep517
 
 python manage.py collectstatic --no-input
 python manage.py migrate
